c85874665041ffbbfc3a1519d33d1f0ea15c6bd0
jonathan <79947845+blckjmmy@users.noreply.github.com>
2021-07-18 11:11:35 -0500

    added guessing game to my javscrpt

--- javas/app.js
+++ javas/app.js
@@ -5,7 +5,9 @@ console.log ('hello world!');
 
 let userName = prompt('Hello Who might you be ?');
 console.log('This is ' + userName);
-alert('Welcome ' + userName + '!');
+alert('Welcome ' + userName + ' you are playing guessing game');
+
+let score = 0;
 
 // 5 questions
 // 1 Q: Did I grow up in maryland?
@@ -119,5 +121,82 @@ mars()
 
 
 
+// question 6
+
+function guessing(){
+alert(userName + ', let\'s play a number guessing game!');
+  let correctAnswer = '77';
+  console.log(correctAnswer);
+  
+  for(let cap = 0; cap < 4; cap++){
+    let userAnswer = prompt('Please enter a number between 1-100. You will have 4 attempts.');
+    console.log(userAnswer);
+  
+    while(userAnswer < 1 || userAnswer > 100) {
+      userAnswer = prompt('Please enter a number from 1-100');
+    }
+    if (userAnswer === correctAnswer){
+      alert('CONGRATULATIONS!! You are correct!');
+      score++;
+      break;
+    }
+    else if (userAnswer < correctAnswer){
+      alert('Too low!');
+    }
+    else if (userAnswer > correctAnswer){
+      alert('Too high!');
+    }
+    else {
+      alert('Something else went wrong.');
+    }
+    console.log(cap);
+    if (cap === 3){
+      alert('I\'m sorry. You did not guess correctly. The correct number is '+ correctAnswer + '.');
+    }
+  }
+}
+
+
+guessing();
+
+
+// question 7
+
+function Shows(){
+  const Array = ['Naruto','Tokyo Revenger','Black clover','Shaman King'];
+  let guesses = 6;
+  let correctshow = false;
+  
+  while(correctshow === false && guesses > 0) {
+    let placeGuess = prompt('can you guess my favorite show' + guesses + ' attempts currently.');
+    let placeGuessLower = placeGuess.toLowerCase();
+    for (let index=0; index<Array.length; index++){
+      if (placeGuessLower === placesArray[index]){
+        alert('CONGRATULATIONS. You are correct!');
+        score++;
+        correctshow = true;
+        break;
+      }
+    }
+    if (correctshow) {
+      alert('Here are the other choices: ' + Array);
+    } if (!correctshow && guesses<= 6 && guesses >1) {
+      alert('Sorry you are incorrect. Please try again.');
+    }
+    if (!correctshow && guesses === 1) {
+      alert('Sorry you got it incorrect. The correct answers were ' + Array);
+    }
+    
+  }
+}
+Shows();
+
+
+
+
+
+
 
 // code ideas from MarquesaAsmussen
+
+
